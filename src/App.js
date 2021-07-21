f4c5c604552b15de21ead0d49c79d647fed9d072
Chris Moon <ChrisM@condon-johnson.local>
2021-07-21 13:57:48 -0700

    Frontend functional

--- src/App.js
+++ src/App.js
@@ -1,142 +1,114 @@
 //import logo from './logo.svg';
 //import './App.css';
+import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Dropdown from 'react-bootstrap/Dropdown';
-
-
-//Connect to the socket server.
-//const io = require("socket.io-client");
 import socketio from "socket.io-client";
-import { render } from '@testing-library/react';
-var socket = socketio.connect('http://192.168.0.31:5000');
-socket.on('connect',console.log('Connected to socket.io server!!'));
 
-/*
-function getDropDownVal2() {
-    var selectedvalue2 = 1;
-    //var selectedvalue2 = document.getElementById("DropDown2").value;
-    indexes['s1'] = selectedvalue2;
-    console.log('User Index Change s1: ', indexes);
-    socket.emit("index_change", indexes);
-};
-function getDropDownVal3() {
-    var selectedvalue3 = 1;
-    //var selectedvalue3 = document.getElementById("DropDown3").value;
-    indexes['s2'] = selectedvalue3;
-    console.log('User Index Change s2: ', indexes);
-    socket.emit("index_change", indexes);
-};
-*/
-//Incoming Temp Readings
-var indexes = {};
-socket.on('indexes', function(msg){
-    indexes['s0'] = msg['s0'];
-    indexes['s1'] = msg['s1'];
-    indexes['s2'] = msg['s2'];
-    console.log('Server Indexes Received: ', indexes);
-    var Sensor0 = Number(indexes['s0']) + 1;
-    //$('#Sensor0').html(Sensor0); 
-    var Sensor1 = Number(indexes['s1']) + 1;
-    //$('#Sensor1').html(Sensor1); 
-    var Sensor2 = Number(indexes['s2']) + 1;
-    //$('#Sensor2').html(Sensor2); 
-});
+const ENDPOINT = "http://192.168.0.31:5000"
+const socket = socketio.connect(ENDPOINT);
+
+
+function SetIndex (props) { 
+  
+  function handleUpdate(e, f) {
+    const indexes = props.indexes
+    console.log(indexes);
+    console.log("Index Updates: ", e, " = ", f);
+    indexes[e] = f;
+    console.log(indexes);
+    socket.emit("index_change", indexes)
+  }
+
+  return(
+    <div>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Edit
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={(e, f) => handleUpdate(props.sensor, 0)}>1</Dropdown.Item>
+          <Dropdown.Item onClick={(e, f) => handleUpdate(props.sensor, 1)}>2</Dropdown.Item>
+          <Dropdown.Item onClick={(e, f) => handleUpdate(props.sensor, 2)}>3</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
+}
 
 
 function App() {
-  var Temp1 = 0;
-  socket.on('newtemps', function(temp_in){
-    console.log('TEMP UPDATE', temp_in);
-    var sensor1 = indexes['s0'];
-    var Temp1 = temp_in[sensor1];
-    //$('#Temp1').html(Temp1);        
-    var sensor2 = indexes['s1'];
-    var Temp2 = temp_in[sensor2];
-    //$('#Temp2').html(Temp2);        
-    var sensor3 = indexes['s2'];
-    var Temp3 = temp_in[sensor3];
-    //$('#Temp3').html(Temp3);
-  });
+  const [temps, setTemps] = useState("");
+  const [indexes, setIndexes] = useState("");
 
-  var selectedvalue1 = '2';
-  function getDropDownVal1(cur_val) {
-    console.log(cur_val);
-    var selectedvalue1 = cur_val;   
-    indexes['s0'] = selectedvalue1;
-    console.log('User Index Change s0: ', indexes);
-    socket.emit("index_change", indexes);
-  };
+  useEffect(() => {
+    socket.on("connect", msg => {
+      console.log("Connect to socket.io server!")
+    });
+    socket.on("indexes", indexes_in => {
+      setIndexes(indexes_in);
+      console.log("Indexes Received: ", indexes_in)
+    });
+    socket.on("newtemps", temp_data => {
+      setTemps(temp_data);
+      console.log("Temps Received: ", temp_data)
+    });
+  }, []);
 
-  //render(){
+  const s0_index = indexes["s0"];
+  const Temp1 = temps[s0_index];
+  const s1_index = indexes["s1"];
+  const Temp2 = temps[s1_index];
+  const s2_index = indexes["s2"];
+  const Temp3 = temps[s2_index];
 
-    //const {Temp1} = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Jumbotron>
-            <h1>MOON BREW CO.</h1>
-            <p>SMOKER PI LIVE FEED & COTROL PANEL</p>
-            <p>Version 1.0.0</p>
-          </Jumbotron>
-
-          <Button variant="primary">Primary</Button>{' '}
-        </header>
-        
-        <table class="table">        
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Sensor Name</th>
-              <th>Sensor ID</th>
-              <th>(toggle)</th>
-              <th>Current Value</th>
-            </tr>
-          </thead>
-          <tbody>          
-            <tr>
-              <td>1</td>
-              <td>Smoker Temp</td>
-              <td>Sensor1</td>
-              <td>
-              <Dropdown>
-                <Dropdown.Toggle variant="success" id="dropdown-basic">
-                  Edit
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item onClick={() => getDropDownVal1(1)}>1</Dropdown.Item>
-                  <Dropdown.Item onClick={() => getDropDownVal1(2)}>2</Dropdown.Item>
-                  <Dropdown.Item onClick={() => getDropDownVal1(3)}>3</Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              </td>
-
-              <td>{Temp1}</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Meat Temp 1</td>
-              <td>Test</td>
-              <td>
-                2
-              </td>                  
-          
-              <td>TEst</td>
-            </tr>          
-            <tr>
-              <td>3</td>
-              <td>Meat Temp 2</td>
-                <td></td>
-                <td>
-                  Test! Fuck yeah!  
-              </td>
-              <td>Test</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-    //}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Jumbotron>
+          <h1>MOON BREW CO.</h1>
+          <p>SMOKER PI LIVE FEED & COTROL PANEL</p>
+          <p>Version 1.0.0</p>
+        </Jumbotron>
+      </header>
+      
+      <table class="table">              
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Sensor Name</th>
+            <th>Sensor ID</th>
+            <th>(toggle)</th>
+            <th>Current Value</th>
+          </tr>
+        </thead>
+        <tbody>          
+          <tr>
+            <td>1</td>
+            <td>Smoker Temp</td>
+            <td>{s0_index + 1}</td>
+            <td><SetIndex sensor = "s0" indexes = {indexes} /></td>
+            <td>{Temp1 + " \xB0F"}</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Meat Temp 1</td>
+            <td>{s1_index + 1}</td>
+            <td><SetIndex sensor = "s1" indexes = {indexes} /></td>        
+            <td>{Temp2 + " \xB0F"}</td>
+          </tr>          
+          <tr>
+            <td>3</td>
+            <td>Meat Temp 2</td>
+            <td>{s2_index + 1}</td>
+            <td><SetIndex sensor = "s2" indexes = {indexes} /></td>
+            <td>{Temp3 + " \xB0F"}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
+
 export default App;
